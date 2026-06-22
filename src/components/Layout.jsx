ad6702418c1e58216101bebf7a95b13beef58e4f
Wilson-son <Wilsonbright1998@gmail.com>
2026-06-22 21:47:11 +0530

    Fix NewsLetter filename case

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 
 import Navebar from "./Navebar";
-import Newsletter from "./Newsletter";
+import NewsLetter from "./NewsLetter";
 import Footer from "./Footer";
 
 export default function Layout() {
